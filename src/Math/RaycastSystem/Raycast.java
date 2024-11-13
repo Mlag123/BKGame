55f84a812634639a50d8e0d1f18964a2497e1bb5
Mlag123 <disw@list.ru>
2024-11-13 04:23:50 +0300

    Refactor Raycast.java class.

--- src/Math/RaycastSystem/Raycast.java
+++ src/Math/RaycastSystem/Raycast.java
@@ -52,20 +52,15 @@ public class Raycast {
      */
     @Deprecated
     public Tags getTagObject(Direction direction, double x, double y) { //fixme
-        //   ArrayList<AbstractObject> abstractObjects = PanelGame.objectArrayList;
-        //    Rectangle r = new Rectangle(10,10,101,0);
+
 
 
         if (direction == Direction.up) {
 
         } else if (direction == Direction.down) {
             for (AbstractObject _object : PanelGame.objectArrayList) {
-                //  long distance = (long) Math.sqrt(Math.pow(_object.getX() - x, 2) - Math.pow((_object.getY()) - y, 2));
-                //System.out.println("pos x = " + x + "| pos y = "+y+"| distance = "+distance+"| Object x = "+_object.getX()+"| Object y = "+ _object.getY());
-                //System.out.println("\n object name = "+_object.getClass().getName());
-                if (Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY() <= 0) {
-                    //      System.out.println("я сосиска"+" "+_object.getClass().getName()+" "+Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY());
 
+                if (Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY() <= 0) {
                     return _object.getTag();
                 }
 
@@ -96,7 +91,6 @@ public class Raycast {
         double pW = PanelGame.player.spriteWidth;
         ArrayList<AbstractObject> objectsList = PanelGame.objectArrayList;
         double distanteToObj = 0;
-        Vector2D unk_object;
 
         AbstractObject gameObject2 = null;
         for (AbstractObject obj : objectsList) {
@@ -106,66 +100,40 @@ public class Raycast {
         }
 
 
-     if (!(gameObject2 == null)){
-         if (direction == Direction.up) { //проверить
-             y--;
-             distanteToObj = (player_vector.getY() + pH) - (gameObject2.object_vector.getY() - gameObject2.spriteHeight);
-             if (distanteToObj <= 0) {
-                 return gameObject2.getTag();
-             }
-         } else if (direction == Direction.down) { //проверить
-             y++;
-             distanteToObj = (player_vector.getY() - pH) - (gameObject2.object_vector.getY() + gameObject2.spriteHeight);
-             if (distanteToObj <= 0) {
-                 return gameObject2.getTag();
-             }
-
-         } else if (direction == Direction.left) {
-             x--;
-             distanteToObj = (player_vector.getX() - pW) - (gameObject2.object_vector.getX() + gameObject2.spriteWidth);
-             if (distanteToObj <= 0) {
-                 return gameObject2.getTag();
-             }
-         } else if (direction == Direction.right) {
-             x++;
-
-             distanteToObj = (player_vector.getX() + pW) - (gameObject2.object_vector.getX() - gameObject2.spriteWidth);
-             if (distanteToObj <= 0) {
-                 return gameObject2.getTag();
-             }
-         }
-     }
-
-/*
-        if (direction == Direction.up) {
-
-        } else if (direction == Direction.down) {
-            for (AbstractObject _object : PanelGame.objectArrayList) {
-                //  long distance = (long) Math.sqrt(Math.pow(_object.getX() - x, 2) - Math.pow((_object.getY()) - y, 2));
-                //System.out.println("pos x = " + x + "| pos y = "+y+"| distance = "+distance+"| Object x = "+_object.getX()+"| Object y = "+ _object.getY());
-                //System.out.println("\n object name = "+_object.getClass().getName());
-                if (Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY() <= 0) {
-                    //      System.out.println("я сосиска"+" "+_object.getClass().getName()+" "+Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY());
-
-                    return _object.getTag();
+        if (!(gameObject2 == null)) {
+            if (direction == Direction.up) { //проверить
+                y--;
+                distanteToObj = (player_vector.getY() + pH) - (gameObject2.object_vector.getY() - gameObject2.spriteHeight);
+                if (distanteToObj <= 0) {
+                    return gameObject2.getTag();
+                }
+            } else if (direction == Direction.down) { //проверить
+                y++;
+                distanteToObj = (player_vector.getY() - pH) - (gameObject2.object_vector.getY() + gameObject2.spriteHeight);
+                if (distanteToObj <= 0) {
+                    return gameObject2.getTag();
                 }
 
+            } else if (direction == Direction.left) {
+                x--;
+                distanteToObj = (player_vector.getX() - pW) - (gameObject2.object_vector.getX() + gameObject2.spriteWidth);
+                if (distanteToObj <= 0) {
+                    return gameObject2.getTag();
+                }
+            } else if (direction == Direction.right) {
+                x++;
+
+                distanteToObj = (player_vector.getX() + pW) - (gameObject2.object_vector.getX() - gameObject2.spriteWidth);
+                if (distanteToObj <= 0) {
+                    return gameObject2.getTag();
+                }
             }
+        }
 
-
-        } else if (direction == Direction.left) {
-
-        } else if (direction == Direction.right) {
-
-        }*/
-
-        // return (AbstractObject) new Object();
         return null;
     }
 
     public boolean isCollide(Direction direction, AbstractEntity abEnt) { //fixme
-        //   ArrayList<AbstractObject> abstractObjects = PanelGame.objectArrayList;
-        //    Rectangle r = new Rectangle(10,10,101,0);
 
 
         if (direction == Direction.up) {
@@ -187,33 +155,7 @@ public class Raycast {
 
         }
 
-        // return (AbstractObject) new Object();
         return false;
     }
 
-
-//         if (Vector2D.subVector(new Vector2D(_object.object_vector.getX() + _object.spriteHeight, _object.object_vector.getY() + _object.spriteHeight), new Vector2D(x, y)).getY() <= 0) {
-//                    //      System.out.println("я сосиска"+" "+_object.getClass().getName()+" "+Vector2D.subVector(_object.object_vector, new Vector2D(x, y)).getY());
-//                    return true;
-//                }
-
-    /*
-            if (isGravity) {
-
-            if (!((object.y - object.spriteHeight) - (y - spriteWidth+23) <= 0)) {
-
-                double _y;
-                _y = y;
-                y = (_y + 20 * 0.1);
-                System.out.println((object.y - object.spriteHeight) - (y - spriteWidth));
-
-
-            } else {
-
-            }
-            //        System.out.println("PLAYER POS : Y"+y);
-
-        }
-
-     */
 }
